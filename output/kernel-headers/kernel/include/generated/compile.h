5b565f375820029e5560f09787c4b5107ae40608
pratik <pratik.yadav@lamresearch.com>
2024-02-29 11:17:46 -0800

    Adding all files that were generated by the shell script

---
+++ output/kernel-headers/kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Mar 24 15:33:41 PDT 2023"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "offboard-NUC7i7DNB"
+#define LINUX_COMPILER "gcc version 6.3.1 20170404 (Linaro GCC 6.3-2017.05)"
